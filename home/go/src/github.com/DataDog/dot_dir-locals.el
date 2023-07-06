de21dd17bca651a3993d237f0165041f8cea2f45
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2023-07-06 22:24:24 +0200

    add emacs dirlocals to global gitignore; set line ending marker AI

--- home/go/src/github.com/DataDog/dot_dir-locals.el
+++ home/go/src/github.com/DataDog/dot_dir-locals.el
@@ -1,2 +1,3 @@
 (("integrations-core" . ((python-mode . ((fill-column . 120)))))
- ("integrations-extras" . ((python-mode . ((fill-column . 120))))))
+ ("integrations-extras" . ((python-mode . ((fill-column . 120)))))
+ ("worktrees/integrations-core" . ((python-mode . ((fill-column . 120))))))
