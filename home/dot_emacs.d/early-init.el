6cb227f8550e9552a8d63f9e19c84e52f609e867
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2026-08-26 21:42:27 +0200

    Disable Emacs native compilation on startup

--- home/dot_emacs.d/early-init.el
+++ home/dot_emacs.d/early-init.el
@@ -2,6 +2,12 @@
 
 (setq package-enable-at-startup nil)
 
+;; Avoid first-startup stalls while Emacs native-compiles straight/use-package/org.
+;; The config is intentionally small enough that JIT native compilation is not
+;; worth making startup feel hung.
+(when (boundp 'native-comp-jit-compilation)
+  (setq native-comp-jit-compilation nil))
+
 (when (fboundp 'menu-bar-mode)
   (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode)
