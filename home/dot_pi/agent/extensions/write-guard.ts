35ba44cfca815c460b72b7a281c7501bc55663ae
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2026-08-27 17:32:23 +0200

    Allow pi writes to Emacs config

--- home/dot_pi/agent/extensions/write-guard.ts
+++ home/dot_pi/agent/extensions/write-guard.ts
@@ -20,6 +20,7 @@ const ALLOWED_DIRS: string[] = [
   "/Users/ilia.kurenkov/src",
   "/Users/ilia.kurenkov/dd",
   nodePath.join(nodeOs.homedir(), ".local/share/chezmoi"),
+  nodePath.join(nodeOs.homedir(), ".emacs.d"),
   "/Users/ilia.kurenkov/.pi",
 ];
 
