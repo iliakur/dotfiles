7afc566343617d3bdd102b43464d99f59a9dabf7
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2026-08-29 14:12:07 +0200

    suppress warning about lexical binding cookies

--- home/dot_emacs.d/early-init.el
+++ home/dot_emacs.d/early-init.el
@@ -2,6 +2,12 @@
 
 (setq package-enable-at-startup nil)
 
+;; Emacs warns when loading Elisp files without a lexical-binding cookie.
+;; That warning type is `(files missing-lexbind-cookie ...)`.
+;; Suppress only that warning class (e.g. old third-party packages).
+(defvar warning-suppress-log-types nil)
+(add-to-list 'warning-suppress-log-types '(files missing-lexbind-cookie))
+
 ;; Avoid first-startup stalls while Emacs native-compiles straight/use-package/org.
 ;; The config is intentionally small enough that JIT native compilation is not
 ;; worth making startup feel hung.
